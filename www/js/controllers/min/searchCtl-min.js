9360cc088ccc499feab49d867a6232fd77fd5776
Benedikt-Alexander Mokroß <benedikt@bhorn.de>
2016-06-09 15:12:47 +0200

    min-Versionen entfernt da defekt.

--- www/js/controllers/min/searchCtl-min.js
+++
@@ -1,1 +0,0 @@
-angular.module("app.controllers").controller("searchCtl",function(c,o,r){c.searchTracks=function(){r.searchFor(c.searchText,"track").then(function(o){console.log(o),c.foundTracks=o.tracks.items},function(c){console.err(c)})}});
